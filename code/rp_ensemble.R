78ebe226277484c27f3368e88456ada8644beaab
dudwnqkr <79184083+qkrwndudata@users.noreply.github.com>
2023-04-08 15:33:58 +0900

    Update rp_ensemble.R

--- code/rp_ensemble.R
+++ code/rp_ensemble.R
@@ -1,26 +1,8 @@
-# library
+## 정의한 ITR fitting 함수와 predict 함수에 RP Ensemble을 적용해 가장 많이 선택된 변수를 뽑아내는 알고리즘 구현 코드입니다.
+
+# library 호출
 library(Matrix)
 library(plyr)
-
-
-# simulation setting
-set.seed(2022021328)
-n <- 500; p <- 50; d <- 10
-
-x <- matrix(runif(n * p, -1, 1), n, p)   # x: 500 x 50
-x1 <- x[,1]; x2 <- x[,2]; x3 <- x[,3]; x5 <- x[,5]
-x6 <- x[,6]; x7 <- x[,7]; x8 <- x[,8]
-
-A <- sample(c(-1, 1), n, replace = TRUE, prob = c(0.5, 0.5))
-
-t1 <- 0.442*(1-x1-x2)*A   # linear: 0.612
-t2 <- (x2 - 0.25*x1^2-1)*A   # poly: 0.604
-t3 <- (0.5 - x1^2 - x2^2)*(x1^2 + x2^2 - 0.3)*A   # sigmoid: 0.624
-t4 <- (1 - x1^3 + exp(x3^2 + x5) +0.6 * x6 - (x7 + x8)^2)*A   # poly: 0.598
-
-Q <- 1 + 2*x1 + x2 + 0.5*x3 + t2
-R <- rtruncnorm(n, a = 0, mean = Q, sd = 1)
-
 
 # define rp generate functions
 generate_projection <- function(d, p){
@@ -32,10 +14,7 @@ generate_projection <- function(d, p){
   return(t(a))
 }
 
-
-
-
-# rp ensemble
+# rp ensemble 적용
 rp_ensemble <- function(x, A, R, B1, B2, d, kernf){   # alpha 나중에 추가
   
   n <- nrow(x)
@@ -55,12 +34,12 @@ rp_ensemble <- function(x, A, R, B1, B2, d, kernf){   # alpha 나중에 추가
       
       classifier <- fit_ITR(z, A, R, 0.5, 0.05, kern = kernf)   
       pred <- predict_ITR(classifier, z)
-      value <- val.func(pred, R)   # value function이 모두 같게 나옴...: 해결해야
+      value <- val.func(pred, R)  
       
       rps[[j]] <- rp
       values[[j]] <- value
     }
-    max.value[[i]] <- rps[[which.max(values)]]
+    max.value[[i]] <- rps[[which.max(values)]]   # value function을 maximize하는 RP 선택
   }
   final <- as.matrix(ldply(max.value, rbind)) # B1개의 행렬을 rbind한 후 colsum으로 결과 확인해 가장 많이 선택된 10개 변수 선택
   vars <-order(colSums(final), decreasing = TRUE)[1:d]
